fb4ce54ec5c7deb002925400b86288286d69ce17
Victor-Johnson <victorjohnson_979@yahoo.com>
2024-11-12 14:37:19 +0100

    Added an append method to my linked list class

--- linkedlist/linked1.py
+++ linkedlist/linked1.py
@@ -10,5 +10,33 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
 
-my_linked_list = LinkedList(4)
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length +=1
+        return True
+
+    # One way of writing a method for appending
+    def appendtolist(self,value):
+        new_node = Node(value)
+        if self.tail:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            self.head = self.tail = new_node
+        self.length +=1
+
+my_linked_list = LinkedList(1)
+my_linked_list.appendtolist(2)
+my_linked_list.print_list()
